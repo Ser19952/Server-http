60ef649ab8bae33f749a1f0ac1762849967c5799
Sergey1995 <Serega19952@rambler.ru>
2024-07-05 15:32:11 +0300

    One commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import src.main.java.org.example.Server;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Server server = new Server(64);
+        server.start(9999);
+    }
+}
